816fe2070a29482dce5956e6f27bc33e383cfa08
BERNARDO MOURTHE E SILVA <bernardo.silva006@cs.unipe.edu.br>
2026-08-31 09:45:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto31_08_26/SobreDisciplinas.kt
@@ -0,0 +1,20 @@
+package com.example.projeto31_08_26
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DisciplinasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disciplinas)
+
+        val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
+
+    }
+}
